ea1d478a37cb0708b94ee7f0e63d48429bb73fa3
Rokym <mirkoivancic.eu@gmail.com>
2022-05-05 21:18:07 +0200

    Security - Login/Logout

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa4wjv5moIbsbLcw5d7',
+        issuer: 'https://dev-69699476.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}

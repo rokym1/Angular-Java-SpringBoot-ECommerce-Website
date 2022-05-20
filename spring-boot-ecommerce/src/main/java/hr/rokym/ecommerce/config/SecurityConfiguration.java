8f19ce1f8f8a3786264c11cbbb5909ba43b1c713
Rokym <mirkoivancic.eu@gmail.com>
2022-05-20 20:34:06 +0200

    Order History - Secure Frontend

--- spring-boot-ecommerce/src/main/java/hr/rokym/ecommerce/config/SecurityConfiguration.java
+++ spring-boot-ecommerce/src/main/java/hr/rokym/ecommerce/config/SecurityConfiguration.java
@@ -20,5 +20,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.cors();
 
         Okta.configureResourceServer401ResponseBody(http);
+
+        http.csrf().disable();
     }
 }
